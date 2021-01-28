405fc6fbef42757588e1ab94bf4f8f8fbfb329d2
David Lewis <lewisde@gmail.com>
2021-01-28 20:59:40 +0000

    Cleaning house.

--- Demos/opengl_triangle.cpp
+++ Demos/opengl_triangle.cpp
@@ -11,14 +11,43 @@
 #endif
 
 #define PI 3.14159265
-int RenderMode = 0;
+int RenderMode = 2;
+int win_width = 640;
+int win_height = 480;
+
+float _angle = 60.0f;
+float _cameraAngle = 30.0f;
 
 using namespace std;
+
+
+void print_text(int x, int y, const char *str)
+{
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(0.0f, win_width, win_height, 0.0f, 0.0f, 1.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+    glRasterPos2f(x, y);
+
+    while(*str) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *str++);
+    }
+
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glPopMatrix();
+}
 
 //Draws the 3D scene
 void drawScene() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+    glColor3f(1.0f, 1.0f, 1.0f);
+    print_text(50, 30, "Spacebar to cycle render mode.");
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     if(RenderMode == 0)
@@ -29,64 +58,33 @@ void drawScene() {
         glPolygonMode( GL_FRONT_AND_BACK, GL_FILL);
     glTranslatef(0.0f, 0.0f, -8.0f);
     glScalef(2.0, 2.0, 0.0);
-
     glPushMatrix();
-    
     glBegin(GL_TRIANGLES);
-
-        // std::clock_t start;
-        // double duration;
-
-        // start = std::clock();
-
-        /* Your algorithm here */
-        float a1 = sqrt(1 - 0.25);
-
-        // duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-
-        // std::cout<<"time for sqrt "<< duration <<'\n';
-
-        
-        // start = std::clock();
-
-        /* Your algorithm here */
+        // float a1 = sqrt(1 - 0.25);
         float a2 = sin(60*(PI/180.0));
 
-        // duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+        // // glColor3f(0.0f, 0.0f, 1.0f); //blue
+        // glVertex3f(-1.25f, 0.0f, 0.0f); //bottom left
+        // // glColor3f(0.0f, 1.0f, 0.0f); //green
+        // glVertex3f(-0.25f, 0.0f, 0.0f); //bottom right
+        // // glColor3f(1.0f, 0.0f, 0.0f); //red
+        // glVertex3f(-0.75f, a1, 0.0f); //top
 
-        // std::cout<<"time for sin "<< duration <<'\n';
-
-
-        glColor3f(0.0f, 0.0f, 1.0f); //blue
-        glVertex3f(-1.25f, 0.0f, 0.0f); //bottom left
-        glColor3f(0.0f, 1.0f, 0.0f); //green
-        glVertex3f(-0.25f, 0.0f, 0.0f); //bottom right
-        glColor3f(1.0f, 0.0f, 0.0f); //red
-        glVertex3f(-0.75f, a1, 0.0f); //top
-        
-        //Triangle
-        glColor3f(0.0f, 0.0f, 1.0f); //blue
+        // glColor3f(0.0f, 0.0f, 1.0f); //blue
         glVertex3f(0.25f, 0.0f, 0.0f); //bottom left
-        glColor3f(0.0f, 1.0f, 0.0f); //green
+        // glColor3f(0.0f, 1.0f, 0.0f); //green
         glVertex3f(1.25f, 0.0f, 0.0f); //bottom right
-        glColor3f(1.0f, 0.0f, 0.0f); //red
+        // glColor3f(1.0f, 0.0f, 0.0f); //red
         glVertex3f(0.75f, a2, 0.0f); //top
-        // glVertex3f(1.0f, 1.0f, 0.0f);
 
-        glColor3f(0.0f, 0.0f, 1.0f); //blue
-        glVertex3f(-0.5f, 0.0f, 0.0f); //bottom left
-        glColor3f(0.0f, 1.0f, 0.0f); //green
-        glVertex3f(0.5f, 0.0f, 0.0f); //bottom right
-        glColor3f(1.0f, 0.0f, 0.0f); //red
-        glVertex3f(0.0f, 1.0, 0.0f); //top
-
-        // bool t =  (a1 == a2);
-        // std::cout << a1 << " - " << a2 << " = " << t << std::endl;
-    
+        // // glColor3f(0.0f, 0.0f, 1.0f); //blue
+        // glVertex3f(-0.5f, 0.0f, 0.0f); //bottom left
+        // // glColor3f(0.0f, 1.0f, 0.0f); //green
+        // glVertex3f(0.5f, 0.0f, 0.0f); //bottom right
+        // // glColor3f(1.0f, 0.0f, 0.0f); //red
+        // glVertex3f(0.0f, 1.0, 0.0f); //top
     glEnd();
-    
     glPopMatrix();
-    
     glutSwapBuffers();
 }
 
@@ -97,8 +95,7 @@ void handleKeypress(unsigned char key, int x, int y) {
             exit(0);
         case ' ':
             RenderMode++;
-            if(RenderMode > 2) {RenderMode = 0;}
-            // std::cout << RenderMode << std::endl;
+            if(RenderMode > 2) {RenderMode = 1;}
             drawScene();
         // default:
             // std::cout << key << std::endl;
@@ -118,9 +115,6 @@ void handleResize(int w, int h) {
     gluPerspective(45.0, (double)w / (double)h, 1.0, 200.0);
 }
 
-float _angle = 60.0f;
-float _cameraAngle = 30.0f;
-
 void update(int value) {
     _angle += 2.0f;
     if (_angle > 360) {
@@ -135,7 +129,7 @@ int main(int argc, char** argv) {
     //Initialize GLUT
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(640, 480);
+    glutInitWindowSize(win_width, win_height);
     
     //Create the window
     glutCreateWindow("OpenGL Triangle");
@@ -151,12 +145,3 @@ int main(int argc, char** argv) {
     glutMainLoop();
     return 0;
 }
-
-
-
-
-
-
-
-
-
